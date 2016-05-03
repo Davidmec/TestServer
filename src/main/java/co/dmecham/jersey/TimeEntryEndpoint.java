975172c9835282a147b7de0bca683a45c4b31b46
david mecham <davidmec@gmail.com>
2016-05-03 08:33:11 -0600

    upgraded the version so it could handle java 8 still have the messageBody writer to configure.

--- src/main/java/co/dmecham/jersey/TimeEntryEndpoint.java
+++ src/main/java/co/dmecham/jersey/TimeEntryEndpoint.java
@@ -2,9 +2,14 @@ package co.dmecham.jersey;
 
 
 
+import co.dmecham.jersey.repository.Repository;
+import co.dmecham.model.Project;
+
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import java.util.UUID;
 
 /**
  * Created by david on 5/2/2016.
@@ -16,6 +21,9 @@ public class TimeEntryEndpoint {
     @GET
     @Path("/{name}")
     public String sayHello(@PathParam("name")String name){
+        Project known  = Repository.project.find(UUID.fromString("b6223ff2-1070-11e6-a148-3e1d05defe78"));
+        Project unKnown = Repository.project.find(UUID.fromString("b6223ff2-1070-11e6-a148-3e1d05defe79"));
+
         return "hello: "+name;
     }
 }
